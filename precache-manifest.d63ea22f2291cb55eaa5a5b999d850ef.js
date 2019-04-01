231f980c779f4cf4a01b1b989f798e581cff501a
Chihyun-Ahn <anchi15@naver.com>
2019-04-01 16:01:05 +0900

    Updates

---
+++ precache-manifest.d63ea22f2291cb55eaa5a5b999d850ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0af9a366c3b5cda55a06",
+    "url": "/git-hub.io/movieapp/static/js/runtime~main.b570a2dc.js"
+  },
+  {
+    "revision": "60fdfe3ab6bcd89348cb",
+    "url": "/git-hub.io/movieapp/static/js/main.2c63e0cc.chunk.js"
+  },
+  {
+    "revision": "a35ee8e669eeb51c0deb",
+    "url": "/git-hub.io/movieapp/static/js/2.639102df.chunk.js"
+  },
+  {
+    "revision": "60fdfe3ab6bcd89348cb",
+    "url": "/git-hub.io/movieapp/static/css/main.c79eb387.chunk.css"
+  },
+  {
+    "revision": "1c33d53c5c471bcff2cc02d44edb86f2",
+    "url": "/git-hub.io/movieapp/index.html"
+  }
+];
